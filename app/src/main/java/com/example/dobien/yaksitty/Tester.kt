142229685fae739d464fedf8861e54b755e55c86
DobLaptop <dobienc82@gmail.com>
2018-11-17 23:17:10 -0800

    Initial commit

---
+++ app/src/main/java/com/example/dobien/yaksitty/Tester.kt
@@ -0,0 +1,12 @@
+package com.example.dobien.yaksitty
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Tester : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
